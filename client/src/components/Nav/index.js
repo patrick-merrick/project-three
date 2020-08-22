8a0ac360b6962b74b1cd6739ab880f987e80f12f
Joe Brimeyer <jbrimeyer@gmail.com>
2020-08-22 00:43:05 -0700

    landing page / splash page.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        DevCon Website
+        DevConnect Website
       </a>
     </nav>
   );
