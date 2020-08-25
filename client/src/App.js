1336a67f2ea033409ba5ebb68d6ce5c47f0f68be
Joe Brimeyer <jbrimeyer@gmail.com>
2020-08-24 20:37:29 -0700

    Added material ui.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Dashboard from "./components/Dashboard/Dashboard";
-import Landing from "./pages/Landing";
-// import Books from "./pages/Books";
-// import Splash from "/componets/Splash"
+import Dashboard from "./components/Dashboard/Dashboard";
+// import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/profile" component={Profile} />
       </div>
     </Router>
   );
