63bade261d0e5a2f20ee98b73df37628bf201a04
Joe Brimeyer <jbrimeyer@gmail.com>
2020-09-02 20:04:25 -0700

    added search functionality.

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Container from "../../components/Container/Container";
 import API from "../../utils/API";
+import SearchDeveloper from "../../components/SearchDeveloper/SearchDeveloper";
 
 class Profile extends Component {
   state = {
@@ -13,27 +14,45 @@ class Profile extends Component {
     summary: "",
     skills: "",
   };
-
+  // joe-brimeyer-15398015
   // When the component mounts, load the next user profile to be displayed
   componentDidMount() {
-    this.loadNextUser("joe-brimeyer-15398015");
+    this.loadNextUser("jon");
   }
 
-  handleBtnClick = (event) => {
-    // Get the data-value of the clicked button
-    const btnType = event.target.attributes.getNamedItem("data-value").value;
-    // Clone this.state to the newState object
-    // We'll modify this object and use it to set our component's state
-    const newState = { ...this.state };
+  // handleBtnClick = (event) => {
+  //   // Get the data-value of the clicked button
+  //   const btnType = event.target.attributes.getNamedItem("data-value").value;
+  //   // Clone this.state to the newState object
+  //   // We'll modify this object and use it to set our component's state
+  //   const newState = { ...this.state };
 
-    this.setState(newState);
-    this.loadNextUser();
+  //   this.setState(newState);
+  //   this.loadNextUser();
+  // };
+  // handleInputChange = (event) => {
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.search);
+    this.loadNextUser(this.state.search);
   };
+
+  handleChange = (event) =>
+    this.setState({ ...this.state, search: event.target.value });
 
   loadNextUser = (userId) => {
     API.linkedInSearch(userId)
       .then((res) =>
         this.setState({
+          ...this.state,
+          // [name]: value,
           id: res.results[0]._id,
           lastName: res.results[0].basicInfo.lastName,
           firstName: res.results[0].basicInfo.firstName,
@@ -49,6 +68,10 @@ class Profile extends Component {
     return (
       <Container>
         <div>
+          <SearchDeveloper
+            handleInputChange={this.handleChange}
+            handleFormSubmit={this.handleFormSubmit}
+          />
           <h1>Profile Page</h1>
           <h4>Profile Information:</h4>
           <p>
@@ -63,13 +86,3 @@ class Profile extends Component {
   }
 }
 export default Profile;
-// function Profile() {
-//   loadSearch = () => {
-//     API.response
-//       .then((res) =>
-//         this.setState({
-//           image: res.data.message,
-//         })
-//       )
-//       .catch((err) => console.log(err));
-//   };
